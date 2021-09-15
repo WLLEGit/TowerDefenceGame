7c833b51d0284093ea76dbde7a68351385c893da
WLLE <342970955@qq.com>
2021-09-15 08:26:55 +0800

    TowerDefenceGame

---
+++ tower.h
@@ -0,0 +1,23 @@
+#ifndef TOWER_H
+#define TOWER_H
+
+
+class Tower
+{
+public:
+    Tower();
+};
+
+class TowerClose : public Tower
+{
+public:
+
+};
+
+class TowerRanged : public Tower
+{
+public:
+
+};
+
+#endif // TOWER_H
